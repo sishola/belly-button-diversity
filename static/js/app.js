f6ea956da9552953d15ef93bd6a2d5db0982908d
sofiat <sofiatishola@gmail.com>
2019-09-23 17:47:52 -0400

    Update app.js

--- static/js/app.js
+++ static/js/app.js
@@ -99,7 +99,73 @@ function optionChanged(selectedValue){
             },
         };
           
-          Plotly.newPlot('bubble', data, layout);
+        Plotly.newPlot('bubble', data, layout);
+
+
+        //Guage Chart
+
+        //Replace NULL wash frequencies with 0
+        var washFreq = datasetMetdata.wfreq === null ? 0 : datasetMetdata.wfreq
+        
+
+        // Trig to calc meter point
+        var degrees = 180-(washFreq*19.5);
+        var radius = .5;
+        var radians = degrees * Math.PI / 180;
+        var x = radius * Math.cos(radians);
+        var y = radius * Math.sin(radians);
+
+        // Path: may have to change to create a better triangle
+        var mainPath = 'M -.0 -0.035 L .0 0.035 L ',
+            pathX = String(x),
+            space = ' ',
+            pathY = String(y),
+            pathEnd = ' Z';
+        var path = mainPath.concat(pathX,space,pathY,pathEnd);
+
+        var data = [{ type: 'category',
+            x: [0], y:[0],
+             marker: {size: 28, color:'850000'},
+             showlegend: false,
+             name: 'Washing Frequency',
+             text: washFreq,
+             hoverinfo: 'text+name'},
+           { values: [1,1,1,1,1,1,1,1,1,9],
+           rotation: 90,
+           
+           text: ["8-9", "7-8", "6-7", "5-6","4-5", "3-4", "2-3", "1-2", "0-1", ""],
+           textinfo: 'text',
+           textposition:'inside',      
+           marker: {colors:["#84B589","#89BC8D",  "#8AC086","#B7CD8F", "#D5E599", "#E5E8B0", "#E9E6C9", "#F4F1E4", 
+                            "#F8F3EC", "#FFFFFF"]},
+           hoverinfo: 'label',
+           hole: .5,
+           type: 'pie',
+           showlegend: false
+         }];
+         
+         var layout = {
+           shapes:[{
+               type: 'path',
+               path: path,
+               fillcolor: '850000',
+               line: {
+                 color: '850000'
+               }
+             }],
+           title: 'Belly Button Washing Frequency',
+           height: 500,
+           width: 600,
+           xaxis: {type:'category',zeroline:false, showticklabels:false,
+                      showgrid: false, range: [-1, 1]},
+           yaxis: {type:'category',zeroline:false, showticklabels:false,
+                      showgrid: false, range: [-1, 1]}
+         };
+
+        Plotly.newPlot("gauge", data, layout);
+
+
+
     })
 }
   
